84d49804683b480be16b392d04b39548a61ab157
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-07 11:58:58 -0500

    Created SQLite Database.

---
+++ my_api/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+
+SQLALCHEMY_DB_URL = "sqlite:///./my_app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DB_URL,
+    connect_args= {
+        "check_same_thread": False,
+    }
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
