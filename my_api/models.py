66bf9f86db84fd8eed8e0126fa52f57c7db8da73
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-07 12:14:23 -0500

    Create Posts table.

---
+++ my_api/models.py
@@ -0,0 +1,15 @@
+"""
+Create SQLAlchemy Tables
+"""
+
+from sqlalchemy import Column, Integer, String
+
+from my_api.database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    body = Column(String, nullable=False)
