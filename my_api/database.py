7b4039ec811686fc726fd82da41fc435debf77c7
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-08 12:29:59 -0500

    working google cloud connection.

--- my_api/database.py
+++ my_api/database.py
@@ -1,14 +1,38 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.engine import Engine
+from google.cloud.sql.connector import Connector, IPTypes
+import os
 
-SQLALCHEMY_DB_URL = "sqlite:///./my_app.db"
 
-engine = create_engine(
-    SQLALCHEMY_DB_URL,
-    connect_args= {
-        "check_same_thread": False,
-    }
-)
+# helper function to return SQLAlchemy connection pool
+def init_connection_pool(connector: Connector) -> Engine:
+    # Python Connector database connection function
+    def getconn():
+        conn = connector.connect(
+            os.environ.get("GCP_SQL_INSTANCE_NAME"), # Cloud SQL Instance Connection Name
+            "pg8000",
+            user=os.environ.get("GCP_SQL_USER"), 
+            password=os.environ.get("GCP_PG_PASSWORD"),
+            db=os.environ.get("GCP_SQL_DB_NAME"),
+            ip_type= IPTypes.PUBLIC,  # IPTypes.PRIVATE for private IP
+            enable_iam_auth=True,
+        )
+        return conn
+
+    SQLALCHEMY_DATABASE_URL = "postgresql+pg8000://"
+
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL , creator=getconn
+    )
+    return engine
+
+# initialize Cloud SQL Python Connector
+connector = Connector()
+
+# create connection pool engine
+engine = init_connection_pool(connector)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
