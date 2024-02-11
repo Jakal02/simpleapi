624ebb628eb12be2ac0ace000b5eb5143e956f66
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-11 12:48:32 -0500

    Dependency created for routes involving meilisearch.

--- my_api/main.py
+++ my_api/main.py
@@ -3,7 +3,10 @@ Main file for my_api.
 """
 from typing import Annotated
 from contextlib import asynccontextmanager
+import os
 from sqlalchemy.orm import Session
+from meilisearch import Client
+from meilisearch.errors import MeilisearchApiError
 from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import PositiveInt
 import my_api.crud as cr
@@ -19,6 +22,19 @@ def get_db():
         db.close()
 
 SessionDep = Annotated[Session, Depends(get_db)]
+
+
+def get_meili_client():
+    client = Client(url=os.environ.get("MEILI_URL"), 
+                    api_key=os.environ.get("MEILI_KEY")
+                )
+    try:
+        client.health()
+        yield client
+    except MeilisearchApiError as e:
+        print("Check something.")
+
+MeiliDep = Annotated[Client, Depends(get_meili_client)]
 
 
 # tables created with alembic at start
@@ -60,3 +76,8 @@ async def delete_post(db: SessionDep, p_id: PositiveInt):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post {p_id} not found.")
     post = cr.delete_post(db, p_id)
     return post
+
+
+@app.get("/search_health/")
+async def check_meili_search(m_client: MeiliDep):
+    return m_client.health()
