15a0bb925febbd766f9d26b85ce1d536c6588716
Jakal02 <gitcontribs@eligrubbs.com>
2024-02-13 15:38:53 -0500

    expanded RetrievePost fields.

--- my_api/schemas.py
+++ my_api/schemas.py
@@ -1,7 +1,13 @@
 """
 Define PyDantic data schemas.
 """
+from datetime import datetime
 from pydantic import BaseModel, PositiveInt
+from pydantic import PlainSerializer
+from typing_extensions import Annotated
+
+
+MyDateTime = Annotated[datetime.datetime, PlainSerializer(lambda x: x.isoformat(), return_type=str)]
 
 class CreatePost(BaseModel):
     """
@@ -16,3 +22,5 @@ class RetrievePost(CreatePost):
     Information returned when a post is retrieved.
     """
     id: PositiveInt
+    date_modified: MyDateTime
+    is_deleted: bool
